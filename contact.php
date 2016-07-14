15d3b6d5dba70c090cf12e8ba4cc7fd73d9b79cb
Jhan Jun-Jie <a22377964@gmail.com>
2016-07-14 23:16:47 +0800

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,32 @@
+<?php
+$field_name = $_POST['your-name'];
+$field_email = $_POST['email'];
+$subject = $_POST['your-subject'];
+$field_message = $_POST['message'];
+
+$mail_to = 'a22377964@gmail.com';
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Message: '.$field_message;
+
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Thank you for the message. I will contact you shortly.');
+		window.location = 'index.html';
+	</script>
+<?php
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Message failed.');
+		window.location = 'index.html';
+	</script>
+<?php
+}
+?>
